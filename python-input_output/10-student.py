8f911225277468643bd3318b5ce78fae3bafbeea
Elham-cyber780 <e.abdulrahm@alustudent.com>
2026-07-23 17:47:36 +0200

    Add 10-student.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Module that defines Student class with filter"""
+
+
+class Student:
+    """A class that defines a student with filter support"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student
+
+        Args:
+            first_name: the first name of the student
+            last_name: the last name of the student
+            age: the age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance
+
+        Args:
+            attrs: list of attribute names to retrieve or None for all
+
+        Returns:
+            dictionary representation of the Student
+        """
+        if isinstance(attrs, list):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
