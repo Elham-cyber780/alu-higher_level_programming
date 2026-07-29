e319566d3a7c32c503a1d97f201515d7bb52e4d3
Elham-cyber780 <e.abdulrahm@alustudent.com>
2026-07-29 16:58:14 +0200

    Fix 0-body_size.sh body size calculation

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a request to a URL and displays the size of the body in bytes
-curl -s "$1" | wc -c | tr -d ' '
+curl -s -o /tmp/curl_body "$1" && wc -c < /tmp/curl_body | tr -d ' '
