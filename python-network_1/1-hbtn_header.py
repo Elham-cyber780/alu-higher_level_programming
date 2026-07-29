334c38d6b46148c93755111ec5ff2e61eef6fbe7
Elham-cyber780 <e.abdulrahm@alustudent.com>
2026-07-29 17:18:46 +0200

    Add 1-hbtn_header.py

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that fetches a URL and displays the X-Request-Id header value"""
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as r:
+        print(r.headers.get('X-Request-Id'))
